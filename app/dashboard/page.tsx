1c4fe20a2852d2cf018639969424f131868fc13e
xmail001 <ISMAILTIKTOK134@gmail.com>
2026-05-14 16:55:50 +0100

    add dashboard page

--- app/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>PropertyFlow Dashboard</h1>
-      <p>Welcome 👋 This is your dashboard</p>
-    </div>
-  );
-}
